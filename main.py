ca06ef65ede22b6772837e070a7356a4966f18a4
guillaume teissier <201614+gteissier@users.noreply.github.com>
2021-02-14 11:05:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,11 +32,28 @@ assert(content)
 days = [d for d in get_days(content)]
 assert(days)
 
-days = days[1:]
-for d in days:
-  #print(d)
+for d in days[1:]:
+  div = d.find_all('div')
+  if len(div) == 0: continue
+  div = div[0]
+  date = div.string.strip('\r\n').rstrip(' ')
+
   sprites = get_sprites(d)
+  # may report no sprite instead of sprite 0 star
+  if len(sprites) == 0: continue
   assert(len(sprites) == 1)
   m = re.search(r'sprite\-etoiles\-(\d+)\-bis', str(sprites[0]))
   assert(m)
-  print(m.group(1))
+  stars = m.group(1)
+
+  img = d.find_all('img')
+  if len(img) != 1: continue
+
+  img = img[0]
+  text = img['alt']
+
+  heights = [m.group(0) for m in re.finditer(r'\dm(50)?', text)]
+  heights = [float(h.replace('m', '.')) for h in heights]
+  if stars == '3' and any(h >= 1.5 for h in heights):
+    desc = '%s hauteurs=%r' % (date, heights)
+    print(desc)
